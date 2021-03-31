0ded4ad50977a4488beeb69f3287275308def757
Hannes Geissler <geissler.hannes@googlemail.com>
2021-03-31 10:01:10 +0200

    added gitignore

---
+++ set-date.sh
@@ -0,0 +1,10 @@
+# in your bashrc, zshrc or equivalent, call the script sh ~/tutorial/set-date.sh
+# ~/tutorial/set-date.sh
+
+# Get time from google server or some other reliable source
+TIME_FROM_SERVER=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //');
+
+# Set stored time
+date +"%d%m%Y%H%M%S" -d "$TIME_FROM_SERVER";
+
+echo "Time updated with success!";
