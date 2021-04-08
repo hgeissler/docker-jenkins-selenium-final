5af62c4b6e07042c2fdcb76888a597ac1d0a56b2
Hannes Geissler <geissler.hannes@googlemail.com>
2021-04-08 05:47:41 +0200

    update set-date

--- set-date.sh
+++ set-date.sh
@@ -2,7 +2,7 @@
 # ~/tutorial/set-date.sh
 
 # Get time from google server or some other reliable source
-TIME_FROM_SERVER=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //');
+TIME_FROM_SERVER=$(curl -v --insecure --silent https://google.de/ 2>&1 | grep Date | sed -e 's/< Date: //');
 
 # Set stored time
 date +"%d%m%Y%H%M%S" -d "$TIME_FROM_SERVER";
